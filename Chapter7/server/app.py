afaa635ff7b7474e81cf8301383bd701d72824b1
xprilion <xprilion@gmail.com>
2019-11-13 01:45:57 +0530

    abc

--- Chapter7/server/app.py
+++ Chapter7/server/app.py
@@ -76,5 +76,9 @@ def generate():
 
     return jsonify(response)
 
+@app.route('/download/<fname>', methods=['GET'])
+def download(fname):
+    return send_file("Output/"+fname, mimetype="audio/midi", as_attachment=True)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
