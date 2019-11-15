ad18a4212d327785621594f372fe7e84c6d24c48
Rimjhim28 <rimjhimbhadani@gmail.com>
2019-11-15 11:03:22 +0530

    add api call

--- Chapter7/flutter_generate_music/lib/play_music.dart
+++ Chapter7/flutter_generate_music/lib/play_music.dart
@@ -1,6 +1,8 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_midi/flutter_midi.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class PlayMusic extends StatefulWidget {
   @override
@@ -34,7 +36,7 @@ class _PlayMusicState extends State<PlayMusic> {
       child: RaisedButton(
         child: Text("Play"),
         onPressed: () {
-           FlutterMidi.playMidiNote(midi: 60);
+           play();
         },
         ),
       );
@@ -45,16 +47,11 @@ class _PlayMusicState extends State<PlayMusic> {
       child: RaisedButton(
         child: Text("Generate Music"),
         onPressed: () {
-          play();
+          load();
         },
         ),
       );
   }
-
-  //  void load(String asset) async {
-  //   FlutterMidi.unmute(); // Optionally Unmute
-  //   ByteData _byte = await rootBundle.load(asset);
-  //   FlutterMidi.prepare(sf2: _byte);
 
       play() async {
         var url = "34.70.80.18:8000/download/output_1573589840.mid";
@@ -62,9 +59,38 @@ class _PlayMusicState extends State<PlayMusic> {
         AudioPlayer.logEnabled = true;
         int result = await audioPlayer.play(url);
         if (result == 1) {
+          print('Success');
       // success
       }
     }
-  //}
+  
 
+  void load() {
+    Future<Response> rsp;
+    rsp = fetchResponse();
+    print('LOAD: $rsp');
+  }
+
+  Future<Response> fetchResponse() async {
+  final response =
+      await http.get('http://34.70.80.18:8000/generate');
+
+  if (response.statusCode == 200) {
+    var v = json.decode(response.body);
+    print('SUCCESS:: ${v['response']}');
+    return Response.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    print('FAILURE');
+    throw Exception('Failed to load post');
+  }
+}
+}
+
+class Response {
+  String response;
+  Response(this.response);
+  factory Response.fromJson(Map<String, dynamic> json) {
+    return Response(json['result']);
+  }
 }
