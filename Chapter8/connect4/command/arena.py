ddc1f344609523ca8ff8ad7a7cb020f43fbafc4d
xprilion <xprilion@gmail.com>
2020-02-18 22:38:20 +0530

    updates

--- Chapter8/connect4/command/arena.py
+++ Chapter8/connect4/command/arena.py
@@ -6,9 +6,7 @@ def arena(argv):
     parser.add_argument('p1', nargs='?', default='human', help='player 1 settings')
     parser.add_argument('p2', nargs='?', default='minimax,6', help='player 2 settings')
     parser.add_argument('-n', '--number', default=1, type=int, help='number of fighting round')
-    parser.add_argument('--history', help='game state from history')
-    parser.add_argument('--first', type=int, help='use first N steps from history')
-    parser.add_argument('--truncate', type=int, help='truncate last N steps from history')
+
     args = parser.parse_args(argv)
 
     from util import player
@@ -20,24 +18,6 @@ def arena(argv):
     from util.arena import Arena
 
     state = State()
-
-    if args.history != None:
-        history = None
-        if args.first != None:
-            history = args.history.split(' ')[1:(args.first + 1)]
-        elif args.truncate != None:
-            history = args.history.split(' ')[1:-args.truncate]
-        if history != None:
-            for action in history:
-                acs = state.getAction()
-                select_action = None
-                for encoded_action, k in acs:
-                    decoded_action = state.actionToString(encoded_action)
-                    if action == decoded_action:
-                        select_action = encoded_action
-                        break
-                assert(select_action != None)
-                state = state.getNextState(select_action)
 
     arena = Arena()
     arena.fight(state, p1, p2, args.number)
