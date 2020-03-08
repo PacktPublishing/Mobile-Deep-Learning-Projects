af98b21fb477dbb74a58f638e699c707ed6951e3
Rimjhim <rimjhi@amazon.com>
2020-03-08 21:22:47 +0530

    final updates

--- Chapter9/flutter_image_super_resolution/lib/image_super_resolution.dart
+++ Chapter9/flutter_image_super_resolution/lib/image_super_resolution.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 import 'package:mime/mime.dart';
 import 'dart:convert';
 import 'package:http_parser/http_parser.dart';
-import 'dart:ui' as ui;
 
 class ImageSuperResolution extends StatefulWidget {
   @override
@@ -13,82 +12,78 @@ class ImageSuperResolution extends StatefulWidget {
 }
 
 class _ImageSuperResolutionState extends State<ImageSuperResolution> {
+  var img1 = Image.asset(
+    'assets/place_holder_image.png',
+    fit: BoxFit.contain,
+  );
+  var img2 = AssetImage('assets/place_holder_image.png');
 
-  var img1 = Image.asset('assets/place_holder_image.png');
-  var img2 = Image.asset('assets/place_holder_image.png');
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Super Resolution')
-      ),
-      body: Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget> [
-          buildImage1(),
-          buildImage2(),
-          buildPickImageButton()
-        ]
+        appBar: AppBar(title: Text('Image Super Resolution')),
+        body: Container(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  buildImageInput(),
+                  buildImageOutput(),
+                  buildPickImageButton()
+                ])));
+  }
+
+  var url =
+      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg';
+
+
+  Widget buildImageInput() {
+    return Expanded(
+      child: Container(
+        width: 200, 
+        height: 200, 
+        child: img1
       )
-    )
     );
   }
 
-
-  Widget buildImage1() {
-    return Container (
-      height: 200, 
-      width: 200,
-      child: img1
+    Widget buildImageOutput() {
+    return Container(
+        child: SizedBox(
+            width: 200, 
+            height: 200, 
+            child: Image(image: NetworkImage(url))
+          )
     );
   }
-
-  Widget buildImage2() {
-    return Container (
-      width: 200,
-      height: 200,
-      child: img2
-      );
-  }
-
-  // Widget buildImageRow() {
-  //   return Container (
-  //     width: 200,
-  //     height: 200,
-  //     child: Image(image: NetworkImage('http://35.223.166.50:8080/download/output_1583568677.png'))
-  //   );
-  // }
 
   Widget buildPickImageButton() {
-    return FloatingActionButton( 
-      elevation: 2 ,
-      onPressed: () => pickImage(), 
-      child: Icon(Icons.camera_alt),);
+    return Container(
+        margin: EdgeInsets.all(8),
+        child: FloatingActionButton(
+          elevation: 8,
+          onPressed: () => pickImage(),
+          child: Icon(Icons.camera_alt),
+        ));
   }
 
-  void pickImage() async{
+  void pickImage() async {
     File pickedImg = await ImagePicker.pickImage(source: ImageSource.gallery);
-    print('PICK Image Called');
     loadImage(pickedImg);
     fetchResponse(pickedImg);
   }
 
   void fetchResponse(File image) async {
-
     print('Fetch Response Called');
-    
+
     final mimeTypeData =
         lookupMimeType(image.path, headerBytes: [0xFF, 0xD8]).split('/');
-    
+
     final imageUploadRequest = http.MultipartRequest(
-        'POST',
-        Uri.parse("http://35.223.166.50:8080/generate"));
-    
+        'POST', Uri.parse("http://35.223.166.50:8080/generate"));
+
     final file = await http.MultipartFile.fromPath('image', image.path,
         contentType: MediaType(mimeTypeData[0], mimeTypeData[1]));
-    
+
     imageUploadRequest.fields['ext'] = mimeTypeData[1];
     imageUploadRequest.files.add(file);
     try {
@@ -97,7 +92,7 @@ class _ImageSuperResolutionState extends State<ImageSuperResolution> {
       print(' Status Code: ${response.statusCode}');
       final Map<String, dynamic> responseData = json.decode(response.body);
       String outputFile = responseData['result'];
-
+      displayResponseImage(outputFile);
     } catch (e) {
       print(e);
       return null;
@@ -105,9 +100,10 @@ class _ImageSuperResolutionState extends State<ImageSuperResolution> {
   }
 
   void displayResponseImage(String outputFile) {
+    print("Updating Image");
     outputFile = 'http://35.223.166.50:8080/download/' + outputFile;
     setState(() {
-      img2 = NetworkImage(outputFile) as Image;
+      url = outputFile;
     });
   }
 
@@ -116,5 +112,4 @@ class _ImageSuperResolutionState extends State<ImageSuperResolution> {
       img1 = Image.file(file);
     });
   }
-
 }
